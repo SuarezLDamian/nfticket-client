39a3214e14aa8095e2e94377a4400e000198383c
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-05-23 20:00:12 -0300

    Initial commit. Project structure.

---
+++ components/footer/footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <div>
+                <p>Copyright © 2022 - All right reserved by NFticket</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { RainbowKitProvider, darkTheme, midnightTheme } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { wagmiClient, chains } from '../config/wagmi';
+import '@rainbow-me/rainbowkit/styles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiProvider>
+  )
 }
 
 export default MyApp
