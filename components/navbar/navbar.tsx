fdbe5b4bf82c535576bba471cb8afdde77ecdd1b
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-05-28 20:32:30 -0300

    Code cleaning.

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -11,19 +11,14 @@ const NavBar = () => {
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <li>
-                            <a>Home</a>
+                            <Link href="/">
+                                <a>Home</a>
+                            </Link>
                         </li>
                         <li tabIndex={0}>
                             <Link href="/events">
-                                <a className="justify-between">
-                                    Eventos
-                                    {/* <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg> */}
-                                </a>
+                                <a>Eventos</a>
                             </Link>
-                            {/* <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul> */}
                             </li>
                         <li>
                             <a>Nosotros</a>
@@ -44,15 +39,8 @@ const NavBar = () => {
                     </li>
                     <li>
                         <Link href="/events">
-                            <a>
-                                Eventos
-                                {/* <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg> */}
-                            </a>
+                            <a>Eventos</a>
                         </Link>
-                        {/* <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul> */}
                     </li>
                     <li>
                         <a>Nosotros</a>
