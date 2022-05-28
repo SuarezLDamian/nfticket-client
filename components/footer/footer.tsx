d10369ca1ce9445bc6f18d07620927fbc8630391
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-05-28 19:04:56 -0300

    Footer text corrected.

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <div>
-                <p>Copyright © 2022 - All right reserved by NFticket</p>
+                <p>Copyright © 2022 - All right reserved by NFTicket</p>
             </div>
         </footer>
     )
