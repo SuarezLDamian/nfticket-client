ef13f844e875f00289378b99e7a8ebd2fc7f01b0
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-05-29 05:03:57 -0300

    New component and layout for single event.

---
+++ components/cardSingle/cardSingle.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ethers } from "ethers";
+import { useEffect } from "react";
+const { abi } = require("../../contracts/NFTicket.json");
+
+interface CardProps {
+    title: string;
+    description: string;
+    image: string;
+    value: number;
+    quantity: number;
+}
+declare let window: any;
+
+const ALCHEMY_PROVIDER = ["matic", "FgJ5VmlekxsmwE6lUZ90pu0i02escfJT"]
+const CONTRACT_ADDRESS = "0xd5E12430159173d93642B302331477c228457082";
+const KEY = process.env.PRIVATE_KEY
+
+const CardSingle = ( { title, description, image, value, quantity }: CardProps ) => {
+
+    const handleClick = async () => {
+
+        // const provider = new ethers.providers.AlchemyProvider(...ALCHEMY_PROVIDER);
+        
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        // console.log("el provider es:", provider)
+        await provider.send("eth_requestAccounts", []); // enviar pop-up en Metamask
+        const signer = provider.getSigner();
+    }
+
+    useEffect(() => {
+        const alchemyProvider = new ethers.providers.AlchemyProvider(...ALCHEMY_PROVIDER);
+        console.log("el alchemyProvider es:", alchemyProvider)
+        // const wallet = new ethers.Wallet(KEY, alchemyProvider);
+        // const signer = wallet.connect(alchemyProvider);
+        
+        // let contract = new ethers.Contract(CONTRACT_ADDRESS, abi, alchemyProvider);
+
+    }, [])
+
+    return (
+        <div className="card m-2 w-60 h-90 sm:w-max sm:card-side bg-base-100 shadow-xl">
+            <figure className="max-h-xs max-w-xs">
+                <img src={image} alt={`Imagen Evento ${title}`} />
+            </figure>
+            <div className="card-body flex justify-center">
+                <h2 className="card-title flex justify-center">{title}</h2>
+                <p className="flex justify-center">Disponibles: {quantity}</p>
+                <p className="flex justify-center">Precio: {value} MATIC</p>
+                <div className="mt-4 card-actions justify-center">
+                    <button onClick={() => handleClick()} className="btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardSingle;
--- pages/event.tsx
+++ pages/event.tsx
@@ -5,17 +5,18 @@ import Link from 'next/link';
 import NavBar from '../components/navbar/navbar';
 import Steps from '../components/steps/steps';
 import Footer from '../components/footer/footer';
+import CardSingle from '../components/cardSingle/cardSingle';
 
 const Event: NextPage = ( ) => {
 
     const router = useRouter();
     const query = router.query;
-    const name = query?.title;
-    const description = query?.description;
-    const image = query?.image;
+    const name = query?.title as string;
+    const description = query?.description as string;
+    const image = query?.image as string;
 
-    const PRICE = "10 MATIC"
-    const QUANTITY = "200"
+    const PRICE = 10;
+    const QUANTITY = 200;
 
     return (
         <>
@@ -23,21 +24,14 @@ const Event: NextPage = ( ) => {
             <div className="flex justify-center">
                 <Steps stepNumber={3}/>
             </div>
-            <div className="m-8 flex justify-center">
-                <h1 className="text-4xl">{name}</h1>
+            <div className="mt-8 flex justify-center">
+                <h1 className="text-4xl sm:text-6xl">{name}</h1>
             </div>
-            <div className="m-4 flex justify-center">
-                <h1 className="text-xl">{description}</h1>
+            <div className="mt-4 flex justify-center">
+                <h2 className="text-xl">{description}</h2>
             </div>
-            <div className="m-4 flex justify-around">
-                {/* <img src={image} alt={`Imagen Evento ${name}`} className='h-96 w-96'/> */}
-            </div>
-            <h1 className="m-4 flex justify-center text-xl">Precio: {PRICE}</h1>
-            <h1 className="m-4 flex justify-center text-xl">Disponibles: {QUANTITY}</h1>
-            <div className="flex justify-center">
-                <Link href="/checkout">
-                    <a className="btn btn-primary m-16">Comprar</a>
-                </Link>
+            <div className="py-4 sm:px-16 xl:px-24 flex justify-center">            
+                <CardSingle title={name} description={description} image={image} value={PRICE} quantity={QUANTITY}/>        
             </div>
             <Footer/>
         </>
