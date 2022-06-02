c0e1954427f5fd19748caa89f8a341ea8ce0054a
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-06-02 05:16:09 -0300

    Fixed image size.

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 interface CardProps {
@@ -7,12 +8,21 @@ interface CardProps {
     contract: string;
 }
 
+const imagelink = "https://billboard.com.ar/wp-content/uploads/2021/07/Captura-de-pantalla-2021-07-26-163139.jpg"
+
 const Card = ( { title, description, image, contract }: CardProps ) => {
 
     return (
         <div className="card my-2 mx-1 w-70 h-90 bg-base-100 shadow-xl">
-            <figure className="max-h-xs max-w-xs">
-                <img src={image} alt={`Imagen Evento ${title}`} />
+            {/* <Image 
+                src={imagelink}
+                alt={`Imagen Evento ${title}`} 
+                width="300px"
+                height="300px"
+                layout="fixed">
+            </Image> */}
+            <figure>
+                <img src={image} alt={`Imagen Evento ${title}`} className="h-80 w-80"/>
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
